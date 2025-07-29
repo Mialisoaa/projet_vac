4e50cbdac344ca3509e09bd372f5621830079f4c
NyAndo <165604753+nyandooooo@users.noreply.github.com>
2025-07-29 20:14:45 +0300

    s

---
+++ pages/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <title>login</title>
+</head>
+<body>
+    <div class="container-fluid">
+
+     <div class="form ">
+            <form action="traitement/tlog.php" method="post" >
+                <div>
+                    <h2>votre mot de passe</h2>
+                    <input type="password" name="password" placeholder="mdp">
+                </div>
+                <div>
+                    <h2>Nom</h2>
+                    <input type="text" name="nom"placeholder="nom">
+                </div>
+                <div class="bouton">
+                    <input type="submit" value="valider">
+                </div>          
+            </form>
+        </div>
+
+    </div>
+     <a href="inscription.php">inscrir</a>
+</body>
+
+</html>
