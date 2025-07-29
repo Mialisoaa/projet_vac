45c9a9adc9f7e35371e314bb683ff6276b40ba0f
NyAndo <165604753+nyandooooo@users.noreply.github.com>
2025-07-29 21:12:28 +0300

    fonction

--- pages/traitement/tinsc.php
+++ pages/traitement/tinsc.php
@@ -5,4 +5,8 @@ $nom=$_POST["nom"];
 $motdepasse=$_POST["password"];
 if ( inscription($nom, $motdepasse)!=-1) {
    echo "<script>alert('inscription ok')</script>";
+    echo "<script> window.location.href = 'login.php';</script>";  
+}else{
+    echo "<script>alert('inscription non ok')</script>";
+     echo "<script> window.location.href = 'login.php';</script>";
 }
