5107769d709f60c2cc5b627ff301dbdb0eadd577
NyAndo <165604753+nyandooooo@users.noreply.github.com>
2025-07-29 20:04:53 +0300

    aa

--- inc/connect.php
+++ inc/connect.php
@@ -1,11 +1,11 @@
 <?php
 ini_set('display_error' ,'1');
-session_start();
+
 function dbconnect()
 {
     static $connect = null;
     if ($connect == null) {
-        $connect = mysqli_connect('localhost', 'root', '', 'employees');
+        $connect = mysqli_connect('localhost', 'root', '', 'blabla');
         if (!$connect){
             die(''. mysqli_connect_error());
         }
