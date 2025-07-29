71d891a577fb34597b35ff20ba7c300cb4215308
NyAndo <165604753+nyandooooo@users.noreply.github.com>
2025-07-29 22:13:08 +0300

    ll

--- pages/traitement/tinsc.php
+++ pages/traitement/tinsc.php
@@ -1,12 +1,16 @@
 <?php
 require "../../inc/fonction.php";
 
-$nom=$_POST["nom"];
-$motdepasse=$_POST["password"];
-if ( inscription($nom, $motdepasse)!=-1) {
-   echo "<script>alert('inscription ok')</script>";
-    echo "<script> window.location.href = 'login.php';</script>";  
-}else{
-    echo "<script>alert('inscription non ok')</script>";
-     echo "<script> window.location.href = 'login.php';</script>";
+$nom = $_POST["nom"];
+$motdepasse = $_POST["password"];
+if (inscription($nom, $motdepasse) != -1) {
+    echo "<script>
+            alert('inscription ok');
+            window.location.href = '../todolist.php';
+        </script>";
+} else {
+    echo "<script>
+            alert('inscription non ok');
+            window.location.href = '../todolist.php';
+        </script>";
 }
