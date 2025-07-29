ee8717a62ed541fbcb23ebd32b90ea1ae123691d
NyAndo <165604753+nyandooooo@users.noreply.github.com>
2025-07-29 21:47:18 +0300

    ee
    v

--- pages/inscription.php
+++ pages/inscription.php
@@ -1,36 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
+     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="UTF-8">
     <link rel="stylesheet" href="../assets/style.css">
-    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
-    <script crc="../bootstrap/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
     <title>incription</title>
 </head>
+
 <body>
     <div class="container-fluid">
 
-     <div class="form ">
+        <div class="form ">
             <form action="traitement/tinsc.php" method="post">
-               
+
                 <div>
                     <h2>Nom</h2>
-                    <input type="text" name="nom"placeholder="nom">
+                    <input type="text" name="nom" placeholder="nom" required>
                 </div>
                 <div>
                     <h2>votre mot de passe</h2>
-                    <input type="password" name="password" placeholder="mdp">
+                    <input type="password" name="password" placeholder="mdp" required>
                 </div>
 
                 <div class="bouton">
                     <input type="submit" value="valider">
-                </div>    
+                </div>
                 <div class="login-link">
-            <a href="login.php">Login</a>
-        </div>      
+                    <a href="login.php">Login</a>
+                </div>
             </form>
         </div>
-       
+
 </html>
